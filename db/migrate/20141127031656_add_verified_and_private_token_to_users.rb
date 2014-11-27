d0323bdb53d5f07955aa5012be2a6acec5bb8af7
Tomwey <tomwey@163.com>
2014-11-27 17:41:58 +0800

    xxxx

--- db/migrate/20141127031656_add_verified_and_private_token_to_users.rb
+++ db/migrate/20141127031656_add_verified_and_private_token_to_users.rb
@@ -1,8 +1,8 @@
 class AddVerifiedAndPrivateTokenToUsers < ActiveRecord::Migration
   def change
     
-    remove_column :users, :verified
-    remove_column :users, :private_token
+    # remove_column :users, :verified
+    # remove_column :users, :private_token
     
     add_column :users, :verified, :boolean, default: true
     add_column :users, :private_token, :string
