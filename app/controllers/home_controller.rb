07c9fefb3d4e0f27337cd84974e014e2540bbaf7
Tomwey <tomwey@163.com>
2014-12-03 22:31:47 +0800

    xxxx

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,14 +4,15 @@ class HomeController < ApplicationController
     @products = Product.hot.order("created_at DESC").limit(6)
     @newsblasts = Newsblast.sorted.limit(5)
     @ads = SidebarAd.sorted.limit(4)
+    @current = 'home_index'
   end
   
   def about
-    
+    @current = 'home_about'
   end
   
   def help
-    
+    @current = 'home_help'
   end
   
 end
