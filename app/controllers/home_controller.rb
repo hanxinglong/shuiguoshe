1558a8965e1a9e0022d30cc786344a21dacd52d4
Tomwey <tomwey@163.com>
2014-12-04 23:43:42 +0800

    xxxx

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     @banners = Banner.sorted.limit(4)
     @products = Product.hot.order("created_at DESC").limit(6)
+    @suggest_products = Product.suggest.limit(6)
     @newsblasts = Newsblast.sorted.limit(5)
     @ads = SidebarAd.sorted.limit(4)
     @current = 'home_index'
