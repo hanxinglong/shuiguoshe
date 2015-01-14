194dd5788431e7282565057b72c1cbf530f76564
tomwey <tomwey@163.com>
2015-01-14 08:58:10 +0800

    xxxx

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -36,7 +36,7 @@ module UsersHelper
     
     if current_user.apartment_id.present?
       edit_html = %(<a onclick="updateApartment()" class="btn btn-sm btn-success">修改</a>)
-      select_html = f.select :apartment_id, Apartment.opened.map { |a| ["#{a.name}（#{a.address}）", a.id, 'user-apartment-id' => current_user.apartment_id ] }, { prompt: "选择小区", selected: current_user.apartment_id}, { class: "form-control", disabled: true, 'data-current' => current_user.apartment_id }
+      select_html = f.select :apartment_id, Apartment.opened.map { |a| ["#{a.name}（#{a.address}）", a.id, 'user-apartment-id' => current_user.apartment_id ] }, { prompt: "选择小区", selected: current_user.apartment_id}, { class: "form-control", 'data-current' => current_user.apartment_id }
       hide_html = %(<div id="hide_order_apartment_input"><input type="hidden" name="order[apartment_id]" value="#{current_user.apartment_id}" /></div>)
     else
       edit_html = %(<a class="btn btn-sm btn-warning" data-user-id="#{current_user.id}" onclick="App.doSaveAddress(this)" id="save_address" disabled>保存到配送设置</a>)
@@ -47,17 +47,39 @@ module UsersHelper
     html = <<-HTML
     <div class="col-sm-4 col-xs-6">
        #{select_html}
-       #{hide_html}
-    </div>
-    <div class="col-sm-2 col-xs-6">
-      <div id="edit-user-apartment">
-        #{edit_html}
-      </div>
     </div>
     HTML
     
     html.html_safe
   end
+  
+  # def deliver_select_tag(f)
+  #   return "" if current_user.blank?
+  #
+  #   if current_user.apartment_id.present?
+  #     edit_html = %(<a onclick="updateApartment()" class="btn btn-sm btn-success">修改</a>)
+  #     select_html = f.select :apartment_id, Apartment.opened.map { |a| ["#{a.name}（#{a.address}）", a.id, 'user-apartment-id' => current_user.apartment_id ] }, { prompt: "选择小区", selected: current_user.apartment_id}, { class: "form-control", disabled: true, 'data-current' => current_user.apartment_id }
+  #     hide_html = %(<div id="hide_order_apartment_input"><input type="hidden" name="order[apartment_id]" value="#{current_user.apartment_id}" /></div>)
+  #   else
+  #     edit_html = %(<a class="btn btn-sm btn-warning" data-user-id="#{current_user.id}" onclick="App.doSaveAddress(this)" id="save_address" disabled>保存到配送设置</a>)
+  #     select_html = f.select :apartment_id, Apartment.opened.map { |a| ["#{a.name}（#{a.address}）", a.id, 'user-apartment-id' => current_user.apartment_id ] }, { prompt: "选择小区", selected: current_user.apartment_id }, { class: "form-control" }
+  #     hide_html = '<div id="hide_order_apartment_input"></div>'
+  #   end
+  #
+  #   html = <<-HTML
+  #   <div class="col-sm-4 col-xs-6">
+  #      #{select_html}
+  #      #{hide_html}
+  #   </div>
+  #   <div class="col-sm-2 col-xs-6">
+  #     <div id="edit-user-apartment">
+  #       #{edit_html}
+  #     </div>
+  #   </div>
+  #   HTML
+  #
+  #   html.html_safe
+  # end
   
   def user_discount_score(total_price)
     return 0 if current_user.blank?
