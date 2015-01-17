1aff341fc634b9df4c1f5a82330f8a64fe4feeed
Tomwey <tomwey@163.com>
2015-01-17 17:33:52 +0800

    xxxx

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,14 @@ class HomeController < ApplicationController
   layout 'help_layout', only: [:order_help, :pay_help, :deliver_help]
   def index
     @banners = Banner.sorted.limit(4)
-    @products = Product.hot.saled.no_discount.order("created_at DESC").limit(6)
+    @products = Product.hot.saled.no_discount.order("sort ASC, created_at DESC").limit(6)
     # @suggest_products = Product.suggest.limit(6)
     
     @sales = Sale.recent
     
     @newsblasts = Newsblast.sorted.limit(5)
     @ads = SidebarAd.sorted.limit(4)
-    @discounted_products = Product.saled.discounted
+    @discounted_products = Product.saled.discounted.order("sort ASC")
     @current = 'home_index'
     fresh_when(etag: [@banners, @sales, @newsblasts, @ads, @products,@discounted_products, @current, SiteConfig.home_title, SiteConfig.home_meta_keywords, SiteConfig.home_meta_description])
     set_seo_meta(SiteConfig.home_title, SiteConfig.home_meta_keywords, SiteConfig.home_meta_description)
