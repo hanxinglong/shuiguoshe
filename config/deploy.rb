8a0f5aa79b11ca73c23efeda586cea889ec50340
Tomwey <tomwey@163.com>
2015-06-02 22:25:36 +0800

    xxxx

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,13 +4,13 @@ lock '3.2.1'
 set :application, 'shuiguoshe'
 set :deploy_user, "deployer"
 
-set :puma_threads, [4, 16]
+set :puma_threads, [0, 16]
 set :puma_workers, 0
 
 set :puma_bind,  "unix://#{shared_path}/tmp/sockets/puma.#{fetch(:application)}.sock"
 set :puma_state, "#{shared_path}/tmp/pids/puma.#{fetch(:application)}.state"
 set :puma_pid,   "#{shared_path}/tmp/pids/puma.#{fetch(:application)}.pid"
-
+# set :puma_conf,  "#{shared_path}/puma.rb"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
 
